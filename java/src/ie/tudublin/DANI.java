98e587c67fffafe89296bbfdcd41f5fd6bcd09aa
Aisha Adebimpe <aishaadebimpe@gmail.com>
2023-04-26 16:57:23 +0100

    made Follow class i think

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -41,4 +41,13 @@ public class DANI extends PApplet {
         textAlign(CENTER, CENTER);
         
 	}
+
+	
 }
+
+
+
+
+
+
+
---
+++ java/src/ie/tudublin/Follow.java
@@ -0,0 +1,19 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+public class Follow {
+    // make a constructor for the Follow class
+   
+
+    
+
+
+    public Follow() {
+        // create an ArrayList of type Follow
+        ArrayList<Follow> follows = new ArrayList<Follow>();
+        // add a new Follow to the ArrayList
+        follows.add(new Follow());
+}
+
+}
